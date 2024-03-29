d2f1989126cc24dcc4761b0bb5ef8469688011b7
sohagraha <sohagrahabd@gmail.com>
2024-03-29 23:21:17 +0600

    setup mongoose express cors parser

---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+    port: process.env.PORT || 3000,
+    database_url: process.env.DATABASE_URL || 'mongodb://localhost:27017/test',
+};
